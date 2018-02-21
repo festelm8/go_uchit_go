90c02944431b6b843552c9c7f0a5b5919e782a83
Sergey Nikulin <s.nikulin@damprodam.ru>
2018-02-21 11:33:51 +0300

    second urok

--- main.go
+++ main.go
@@ -3,9 +3,23 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println(cock(14, 17))
+	fmt.Println()
 }
 
-func cock(a,b int)(int){ //в скобках указываем получаемые данные, во вторых скобках тип возвращаемых данных
-	return a+b
-}
+//a := []string{"kok","world"} // создание списка со строками
+//a := make([]string, 5)  //альтернатив способ объявления списка (в примере с 5 пустыми строками)
+
+//for i:=0; i<len(a); i++ { //цикл хуле
+//	fmt.Println(a[i])
+//}
+
+//for k,v := range a{ //в связке с range получаеться этакий foreach
+//	fmt.Println(k)
+//	fmt.Println(v)
+//}
+
+//fmt.Println(cock(14, 17))
+//func cock(a,b int)(int){ //в скобках указываем получаемые данные, во вторых скобках тип возвращаемых данных
+//	return a+b
+//}
+
